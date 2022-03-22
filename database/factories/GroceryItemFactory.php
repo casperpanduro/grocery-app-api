4c2f67cd5b0a34da4b7a9514ea16776a97521fc1
Casper Panduro <cap@checkmate.dk>
2022-03-22 22:36:54 +0100

    factory for grocery item

--- database/factories/GroceryItemFactory.php
+++ database/factories/GroceryItemFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\GroceryList;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class GroceryItemFactory extends Factory
@@ -14,7 +16,9 @@ class GroceryItemFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->text(20),
+            'user_id' => User::factory()->create(),
+            'grocery_list_id' => GroceryList::factory()->create()
         ];
     }
 }
